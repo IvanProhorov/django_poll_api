dcb006c3be0db7e64aaad74e29ba6f3517d11c06
iapro <prokh68@yandex.ru>
2021-04-28 21:51:09 +1000

    Refactor code

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,8 @@ from .views import (ChoicesView, PollsView, VotesView)
 
 api_v1 = [
     path('votes/<int:user_id>/', VotesView.as_view(), name='get_votes'),
-    path('choices/<int:choice_id>/', ChoicesView.as_view(), name='get_choices'),
+    path('choices/<int:choice_id>/', ChoicesView.as_view(),
+         name='get_choices'),
     path('polls/<int:poll_id>/', PollsView.as_view(), name='get_polls'),
 
 ]
